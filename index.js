0e789ba7944395761292f6e537b7f283fe85e103
sid370578 <sid370578@gmail.com>
2025-07-11 22:04:29 +0530

    change done by d4

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("siddiq shaikh playing with git")
 //d3 changes
 // Developer 2 added this line
+//developer 4 changes kr raha hai pehla pull le liya master sa uska baad change 
+//so koi conflict nhi hai 
